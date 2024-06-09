0fd448c4c85c2bdc0546ca7b2f36bc65e3510b4b
Kaitlyn <kd4accounts@hotmail.com>
2024-06-09 15:06:36 -0400

    Initial Commit

---
+++ CPSC5330-3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CPSC5330-3
+//
+//  Created by user260677 on 6/9/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ CPSC5330-3UITests/CPSC5330_3UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  CPSC5330_3UITestsLaunchTests.swift
+//  CPSC5330-3UITests
+//
+//  Created by user260677 on 6/9/24.
+//
+
+import XCTest
+
+final class CPSC5330_3UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
